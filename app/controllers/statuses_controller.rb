26547914265916b2a748dfe9a481096277d1539e
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-20 12:26:29 -0700

    provide content in views

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -11,7 +11,7 @@ class StatusesController < ApplicationController
   def create
     @status = Status.new(status_params)
     if @status.save
-      redirect_to status_path
+      redirect_to root_path
     end
   end
 
