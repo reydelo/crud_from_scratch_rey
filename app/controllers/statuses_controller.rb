23196ca900b73f04af5b35a86cffc066c4686732
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-20 11:58:27 -0700

    create statuses controller and routes

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,32 @@
+class StatusesController < Application Controller
+
+  def index
+    @statuses = Status.all
+  end
+
+  def new
+    @status = Status.new
+  end
+
+  def create
+    @status = Status.new(status_params)
+    if @status.save
+      redirect_to status_path
+    end
+  end
+
+  def show
+    @status = Status.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def status_params
+    params.require(:status).permit(:status, :user, :likes)
+
+end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,5 @@
 class Status < ActiveRecord::Base
   validates :status, :user, presence: true
-  validates_length_of :status, :maximum => 140, :message => "please shorten your status dear"
-  validates_length_of :user, :maximum => 15, :message => "please shorten your username dear"
+  validates_length_of :status, :within => 1..140, :too_long => "please shorten your status dear", :too_short => "please say something, ANYTHING, in your status dear"
+  validates_length_of :user, :within => 1..15, :too_short => "please add another character dear", :too_long => "please pick a shorter username dear"
 end
