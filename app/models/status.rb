af3a010224a8e4960315a7ad02762fcc2d2f8b67
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-20 11:39:02 -0700

    add validations and tests to statuses

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,5 @@
 class Status < ActiveRecord::Base
+  validates :status, :user, presence: true
+  validates_length_of :status, :maximum => 140, :message => "please shorten your status dear"
+  validates_length_of :user, :maximum => 15, :message => "please shorten your username dear"
 end
--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Status, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a status with a text and user" do
+    status = Status.create(status: "Shout out to my gSchool homies", user: 'ReyDeLo')
+    expect(status.status).to eql 'Shout out to my gSchool homies'
+    expect(status.user).to eql 'ReyDeLo'
+  end
+
+  it "require status and user fields" do
+    status = Status.create(status: "I've got connections")
+    expect(status).to be_invalid
+  end
+
+  it "require maximum length of user and status" do
+    status = Status.create(status: "Rose went to Eve's house but she wasn't there. But Eve's father was. Alone. One thing led to another. He got 10 years. - Rachel Johnson",
+    user: 'ABCDEFGHIJKLMNOP')
+    expect(status).to be_invalid
+  end
 end
